c5e29226503a196a3f26e3765cacd65e82a11663
benyasin <Bingying722>
2021-05-30 14:23:09 +0800

    upd: using @storybook/addon-postcss

--- default/.storybook/main.js
+++ default/.storybook/main.js
@@ -1,28 +1,24 @@
 module.exports = {
-	stories: [
-		'./*.stories.@(js|jsx|ts|tsx|mdx)',
-		'../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-	],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-	/**
-	 * @param {import('webpack/index').Configuration} config
-	 */
-	webpackFinal: async (config, { configType }) => {
-		// `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-		// You can change the configuration based on that.
-		// 'PRODUCTION' is used when building the static version of storybook.
+  stories: ['./*.stories.@(js|jsx|ts|tsx|mdx)', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  /**
+   * @param {import('webpack/index').Configuration} config
+   */
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
 
-		// Make whatever fine-grained changes you need
-		const loaderIndex = config.module.rules.findIndex(
-			(val) => val.loader && val.loader.includes('svelte-loader')
-		);
-		config.module.rules[loaderIndex].options = {
-			preprocess: require('svelte-preprocess')(
-				require('../svelte-preprocess.config').sveltePreprocessConfig
-			),
-		};
+    // Make whatever fine-grained changes you need
+    const loaderIndex = config.module.rules.findIndex(
+      (val) => val.loader && val.loader.includes('svelte-loader'),
+    );
+    config.module.rules[loaderIndex].options = {
+      preprocess: require('svelte-preprocess')(),
+      //require('../svelte-preprocess.config').sveltePreprocessConfig
+    };
 
-		// Return the altered config
-		return config;
-	},
+    // Return the altered config
+    return config;
+  },
 };
